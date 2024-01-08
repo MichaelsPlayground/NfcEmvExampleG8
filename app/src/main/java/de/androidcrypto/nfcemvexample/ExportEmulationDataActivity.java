71808c9e79cf8ed17289b4c827aa6d1320878bb7
michaelfehr <michael_fehrgmx.de>
2024-01-08 01:51:59 +0100

    updated

--- app/src/main/java/de/androidcrypto/nfcemvexample/ExportEmulationDataActivity.java
+++ app/src/main/java/de/androidcrypto/nfcemvexample/ExportEmulationDataActivity.java
@@ -593,6 +593,25 @@ public class ExportEmulationDataActivity extends AppCompatActivity implements Nf
 
     }
 
+    // see https://en.wikipedia.org/wiki/Luhn_algorithm
+    public static boolean isValidLuhn(String number) {
+        int checksum = Character.getNumericValue(number.charAt(number.length() - 1));
+        int total = 0;
+
+        for (int i = number.length() - 2; i >= 0; i--) {
+            int sum = 0;
+            int digit = Character.getNumericValue(number.charAt(i));
+            if (i % 2 == 0) {
+                digit *= 2;
+            }
+
+            sum = digit / 10 + digit % 10;
+            total += sum;
+        }
+
+        return 10 - total % 10 == checksum;
+    }
+
     /**
      * section for anonymize the export string
      */
